7d5e8ca734b47a6d44f679d15a1bd54a19d9de58
sanjeevkumar-j-v <sanjeevkumarjv@gmail.com>
2021-03-01 23:07:10 +0530

    Add passport dependencies

--- app.js
+++ app.js
@@ -1,46 +1,104 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const db = require("./config/mongoose");
+// used for session cookie
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
+// const passportGoogle = require('./config/passport-google-oauth2-strategy');
+// const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo").default;
 
 const expressLayouts = require("express-ejs-layouts");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 
 var app = express();
 
 app.use(expressLayouts);
 
-app.set("layout extractStyles", true);
-app.set("layout extractScripts", true);
+// app.set("layout extractStyles", true);
+// app.set("layout extractScripts", true);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
+// app.use(
+//   session({
+//     name: "dailyshop",
+//     // todo change the secret before deployment
+//     secret: "blahblah",
+//     saveUninitialized: false,
+//     resave: false,
+//     cookie: {
+//       maxAge: 1000 * 60 * 100,
+//     },
+//     store: new MongoStore(
+//       {
+//         mongooseConnection: db,
+//         autoRemove: "disabled",
+//       },
+//       function (err) {
+//         console.log(err || "connect-mongodb setup ok");
+//       }
+//     ),
+//   })
+// );
+
+app.use(
+  session({
+    secret: "blahblah",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+    //     store: new MongoStore(
+    //       {
+    //         mongooseConnection: db,
+    //         autoRemove: "disabled",
+    //       },
+    //       function (err) {
+    //         console.log(err || "connect-mongodb setup ok");
+    //       }
+    //     ),
+    store: MongoStore.create({
+      mongoUrl: "mongodb://localhost/daily-shop-store",
+    }),
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
